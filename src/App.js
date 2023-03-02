d2400a1b0877cda497120d9b3322c176894f50e4
Michael Baldanza <michael.snider.baldanza@gmail.com>
2023-03-02 10:07:16 -0500

    add background-color and font-family styles, add footer and nav

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="nonfooter" class="container-fluid">
+        <nav class="navbar navbar-expand-sm">
+          <div>
+            <a href="" class="navbar-brand">Glossi</a>
+          </div>
+        </nav>
+      </div>
+      <footer class="container-fluid">With thanks to <a href="https://dictionaryapi.dev/" class="link-primary text-decoration-none">
+        the Free Dictionary API</a> and <a href="https://dictionaryapi.com/products/index" class="link-primary text-decoration-none">Merriam-Webster's Dictionary API</a>.
+        <div>
+          <img height="50" width="auto" src="https://dictionaryapi.com/images/info/branding-guidelines/MWLogo_LightBG_120x120_2x.png"></img>
+        </div>
+      </footer>
     </div>
   );
 }
