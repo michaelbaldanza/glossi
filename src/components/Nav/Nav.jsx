3f5e5e87e2a27f72cf4b1a7e54e8d841fdc67690
Michael Baldanza <michael.snider.baldanza@gmail.com>
2023-07-18 10:47:23 -0400

    simplify navbar css, selecting elements button and anchor elements instead of too many classes. add custom navbar toggler icon and make colours consistent with nav colours

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,6 +6,10 @@ import { logout } from '../../services/users';
 export default function Nav(props) {
   const [user, setUser] = props.user;
   const navigate = useNavigate();
+  // try togglerIcon with 1.5emx1.5em dims
+  const togglerIcon = <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>;
 
   function makeScrollUI() {
     return (
@@ -80,7 +84,10 @@ export default function Nav(props) {
       <div className="container-fluid">
       <Link className="navbar-brand" to={'/'}>Glossi</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        {/* <span className="navbar-toggler-icon"></span> */}
+        <span className="custom-toggler-icon">
+          {togglerIcon}
+        </span>
       </button>
       <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
