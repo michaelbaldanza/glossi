311c7c5b83991816f05d6d08c273ba7463cce980
Michael Baldanza <michael.snider.baldanza@gmail.com>
2023-07-21 17:34:06 -0400

    split Infobox's makeInfobox* functions into separate components. add await to save() calls in controllers/cards create to avoid error when adding card to newly created deck

---
+++ src/components/Scroll/WordWrapper/Infobox/Body/Body.jsx
@@ -0,0 +1,41 @@
+import CardForm from './CardForm';
+import Dictionary from './Dictionary/Dictionary';
+import { refOrder } from '../../../../../services/dictionaries';
+
+export default function Body(props) {
+  const activeDict = props.activeDict;
+  const [addView, setAddview] = props.addView;
+  const decks = props.decks;
+  const dictionaries = props.dictionaries;
+  const dictProps = props.dictProps;
+
+  console.log(`The value of addView is`)
+  console.log(addView);
+
+  function makeCardForm() {
+    console.log(`itting`)
+    return (<CardForm
+      activeDict={activeDict}
+      scrollId={props.scrollId}
+      entry={addView[0]}
+      decks={decks}
+      wordId={props.wordId}
+    />);
+  }
+
+  function makeDictionaryPanels() {
+    return refOrder.map((dictabbr, idx0) => (
+      <div
+        key={`${dictabbr}-${idx0}`}
+        style={{'display': activeDict === dictionaries[dictabbr].name ? 'block' : 'none'}}
+      >
+        <Dictionary
+          mostRecent={dictionaries[dictabbr]}
+          {...dictProps}
+        />
+      </div>
+    ));
+  }
+  
+  return addView.length ? makeCardForm() : makeDictionaryPanels();
+}
---
+++ src/components/Scroll/WordWrapper/Infobox/Body/Dictionary/Entry.jsx
@@ -0,0 +1,135 @@
+import { Fragment } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import Word from '../../../Word';
+import { breakLines, clipTags, escape, isLast } from '../../../../../../services/helpers';
+import { BTN_CLASSES } from '../../../../../../services/constants';
+
+export default function Entry(props) {
+  const [selLang, setSelLang] = props.selLang;
+  const [user, setUser] = useOutletContext();
+  const [addView, setAddView] = props.addView;
+  const [currentIdx, setCurrentIdx] = props.currentIdx;
+  const api = props.api.toLowerCase().replace(' ', '-');
+  const entry = props.entry;
+  const quarry = props.quarry;
+
+  function makePartOfSpeechHeading() {
+    if (!entry.partOfSpeech) return;
+    function makeAddViewButton() {
+      if (!user || props.abbr === 'mw' || props.abbr === 'odus') return;
+
+      function handleClick(e) {
+        e.stopPropagation();
+        entry.headword = quarry;
+        if (selLang) entry.languageCode = selLang;
+        setAddView([entry])
+      }
+
+      return (
+        <button
+            className={BTN_CLASSES}
+            onClick={(e) => handleClick(e)}
+        >
+          +
+        </button>
+      );
+    }
+    return (<div style={{'display': 'flex', 'alignItems': 'center'}}>
+      <h6 className="faded" style={{'fontSize': 'small'}}>
+      {entry.partOfSpeech.toLowerCase()}
+      {props.lang === 'other' ? ' - ' + entry.language : ''}
+      
+    </h6>
+    {makeAddViewButton()}
+    </div>)
+  }
+
+  function makeSynsOrAnts(synsOrAnts) {
+    if (!entry[synsOrAnts] || !entry[synsOrAnts].length) return;
+    return <>
+      <h6
+        className="faded" style={{'fontSize': 'smaller'}}
+      >
+        {synsOrAnts}
+      </h6>
+      <div>
+        {
+          entry[synsOrAnts].map((synOrAnt, idx0) => {
+            return (
+              <Fragment key={`${synOrAnt}-${idx0}`}>
+                <Word
+                  currentIdx={[currentIdx, setCurrentIdx]}
+                  clickThroughHistory={props.clickThroughHistory}
+                  isBoxWord={true}
+                  wordId={`${synOrAnt}-${idx0}`}
+                  word={synOrAnt}
+                  handleRef={props.handleRef}
+                  selLang={[selLang, setSelLang]}
+                />
+                {
+                  isLast(idx0, entry[synsOrAnts]) ?
+                  ''
+                  :
+                  ', '
+                } 
+              </Fragment>
+            )
+          })
+        }
+      </div>
+    </>
+  }
+
+  return (
+    <div
+      key={`${api}-entry-${props.idx1}`}
+      style={{
+        'display': props.lang ? selLang === props.lang ? 'block' : 'none' : 'block',
+        'borderBottom': props.isLast ? '' : 'dashed grey 0.25px',
+      }}
+      className={`entry-container ${api}`}
+    >
+      {
+        (entry.headword && entry.headword !== quarry) ?
+        <h6>{entry.headword}</h6> : ''
+      }
+      {makePartOfSpeechHeading()}
+      <ol>
+        {
+          entry.definitions.map((def, idx2) => {
+            function makeLi(defOrDefinition) {
+              return (<li key={`sense-${props.idx1}-${idx2}`}>
+                {
+                  breakLines(defOrDefinition).map((line, idx2p5) => (
+                    line.split(/(\s|\/)/g).map((word, idx3) => (
+                      escape(word) ? word :
+                      <Word
+                        currentIdx={[currentIdx, setCurrentIdx]}
+                        clickThroughHistory={props.clickThroughHistory}
+                        handleRef={props.handleRef}
+                        isBoxWord={true}
+                        isQuarry={word === quarry ? true : false}
+                        key={`${word}-${idx2}-${idx3}`}
+                        selLang={[selLang, setSelLang]}
+                        wordId={`${word}-${idx2}-${idx3}`}
+                        word={word}
+                      />
+                    ))
+                  ))
+                }
+              </li>);
+            }
+
+            return (
+              def.hasOwnProperty('definition') ?
+              def.definition.length ? makeLi(clipTags(def.definition)) : ''
+              :
+              makeLi(def)
+          )})
+        }
+      </ol>
+      {makeSynsOrAnts('synonyms')}
+      {makeSynsOrAnts('antonyms')}
+    </div>
+  );
+}
